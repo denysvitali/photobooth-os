3a3e70b9785c26a25b66b116fd1539ec17b92490
Denys Vitali <denys@denv.it>
2025-04-01 00:36:25 +0200

    fix: use .env

--- build_image.sh
+++ build_image.sh
@@ -5,13 +5,7 @@ DOCKER_IMAGE=${1:-registry.gitlab.com/raspi-alpine/builder/master:latest}
 docker run \
   --rm \
   -it \
-  -e "DEFAULT_TIMEZONE=Europe/Zurich" \
-  -e "DEFAULT_KERNEL_MODULES=*" \
-  -e "ARCH=aarch64" \
-  -e "ALPINE_BRANCH=v3.20" \
-  -e "DEFAULT_HOSTNAME=photobooth" \
-  -e "SIZE_ROOT_FS=4G" \
-  -e "SIZE_ROOT_PART=4G" \
+  --env-file .env \
   -v "$PWD":/input \
   -v "$PWD":/output \
   "$DOCKER_IMAGE"
